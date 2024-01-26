8dc5537cabd540a634968b9efaaba57055c65092
Offical Page <34690044+NikSton@users.noreply.github.com>
2024-01-26 17:32:30 +0400

    Delete Simple Small Scripts/MineSweeper directory

--- Simple Small Scripts/MineSweeper/lua/autorun/minesweeper_init.lua
+++
@@ -1,9 +0,0 @@
-MineSweeper = MineSweeper or {}
-include("minesweeper/sh_config.lua")
-if SERVER then
-	AddCSLuaFile("minesweeper/sh_config.lua")
-	AddCSLuaFile("minesweeper/cl_init.lua")
-	include("minesweeper/sv_init.lua")
-else
-	include("minesweeper/cl_init.lua")
-end
